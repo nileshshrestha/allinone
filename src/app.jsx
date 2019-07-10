2293030fc3cf1ed58ca45a9115bab7e1601ce6c2
Nilesh Shrestha <Nilesh.Shrestha@thestreet.com>
2019-07-10 12:38:44 -0400

    stomp endpoint added

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import SockJsClient from 'react-stomp';
 
 export default class App extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+          msg : 'hi'
+        }
     }
+
+    sendMessage = (msg) => {
+      this.clientRef.sendMessage('/topic/greeting', msg);
+    }
+
     render(){
+
         return(
+
             <React.Fragment>
+              <SockJsClient url='http://localhost:7001/websocket' topics={['/topic/greeting']}
+              onMessage={(msg) => { console.log(msg.msg); this.setState({msg:msg.msg}); }}
+              ref={ (client) => { this.clientRef = client }} />
                 <header className="header">Defualt Header</header>
-                <section className="body">
-                    <h1 className="title">Hello Stomp</h1>
-                </section>
+                <section className="body">{this.state.msg}</section>
                 <footer className="header">App Footer</footer>
             </React.Fragment>
         )
